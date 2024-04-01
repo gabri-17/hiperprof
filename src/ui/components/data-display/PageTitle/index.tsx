3442ef808aee07c9db57263b1a82c82086d34dbf
Gabriel Antonio <ga170120@gmail.com>
2024-04-01 16:00:21 -0300

    Criando componente de loading

--- src/ui/components/data-display/PageTitle/index.tsx
+++ src/ui/components/data-display/PageTitle/index.tsx
@@ -1,18 +1,27 @@
-import { PageSubtitleStyled, PageTitleContainer, PageTitleStyled } from "./styles";
+import {
+  PageSubtitleStyled,
+  PageTitleContainer,
+  PageTitleStyled,
+} from "./styles";
+import { TypographyProps } from "@mui/material";
 
-export interface PageTitleProps{
-    title: string;
-    subtitle?:string // ? : dizer que é um campo opcional.
+export interface PageTitleProps extends Omit<TypographyProps, "title"> {
+  title: string;
+  subtitle?: string; // ? : dizer que é um campo opcional.
 }
 
 // ReactFC: function components e vai receberPageTitleProps como propriedades que podem ser pegas.
-const PageTitle:React.FC<PageTitleProps> = ({title, subtitle}) =>{
-    return (
-        <PageTitleContainer>
-            <PageTitleStyled color={'primary'}>{title}</PageTitleStyled>
-            <PageSubtitleStyled>{subtitle}</PageSubtitleStyled>
-        </PageTitleContainer>
-    );
-}
+const PageTitle: React.FC<PageTitleProps> = ({
+  title,
+  subtitle,
+  color = "primary",
+}) => {
+  return (
+    <PageTitleContainer>
+      <PageTitleStyled color={color}>{title}</PageTitleStyled>
+      <PageSubtitleStyled>{subtitle}</PageSubtitleStyled>
+    </PageTitleContainer>
+  );
+};
 
 export default PageTitle;
