5534739d7acf0a2987f4b174102f7f2edabce8b7
Gabriel Antonio <ga170120@gmail.com>
2024-04-08 16:07:56 -0300

    Instalando react input mask

---
+++ src/data/@types/inputMask.d.ts
@@ -0,0 +1,5 @@
+// .d: declarar módulos de tipos na aplicação.
+declare module 'react-input-mask' {
+    export class ReactInputMask extends React.Component<any>{}
+    export default ReactInputMask;
+}
