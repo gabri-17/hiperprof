5efcbd26672933d6d3d6fa21a970c51dd40dde26
Gabriel Antonio <ga170120@gmail.com>
2024-04-12 18:08:22 -0300

    Criando rota de cadastro

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -23,5 +23,12 @@ export const Router = {
         },
         icon: "",
     },
+    cadastroProfessor: {
+        name: "/professor/cadastro-professor",
+        push: function (router: NextRouter){
+            router.push({pathname:this.name });
+        },
+        icon: "",
+    },
     
 };
