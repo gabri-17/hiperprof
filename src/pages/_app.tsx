42c18085daa4dc32b1efcb9515832b90ff44948f
Gabriel Antonio <ga170120@gmail.com>
2024-04-12 18:07:23 -0300

    Adicionando tema na aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@styles/globals.css'
-import type { AppProps } from 'next/app'
+import Base from "@components/surfaces/Base";
+import { ThemeProvider } from "@emotion/react";
+import "@styles/globals.css";
+import type { AppProps } from "next/app";
+import theme from "ui/theme/light-theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    //Para acessar em toda a aplicação é preciso passar o tema como pai de todos os componentes.
+    <ThemeProvider theme={theme}>
+      <Base>
+        <Component {...pageProps} />
+      </Base>
+    </ThemeProvider>
+  );
 }
