79d5269225b0ce1839ae5a6fc0a5215f81ea494e
Gabriel Antonio <ga170120@gmail.com>
2024-04-15 21:13:51 -0300

    Criando interface de login

---
+++ src/data/@types/login.ts
@@ -0,0 +1,12 @@
+// Login vai retornar uma response.
+export interface LoginInterface{
+    email: string,
+    password: string
+}
+
+export interface LoginErroInterface extends LoginInterface{}
+
+export interface ResponseLoginInterface{
+    refresh_token: string;
+    token: string;
+}
