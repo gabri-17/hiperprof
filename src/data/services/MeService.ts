b561a30331febf7aa5d41e909712d6d298bf66f1
Gabriel Antonio <ga170120@gmail.com>
2024-04-23 18:58:52 -0300

    Criando serviço para buscar professor

---
+++ src/data/services/MeService.ts
@@ -0,0 +1,10 @@
+import { ProfessorInterface } from "@data/@types/professor";
+import { ApiService } from "./ApiService";
+
+export const getUser = async () =>
+  await ApiService.get<ProfessorInterface>("/api/me", {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token_hiperprof")}`,
+    },
+  });
+
