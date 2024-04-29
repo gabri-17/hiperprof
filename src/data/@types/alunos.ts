43b462cfd1a6e878f9e142198840d577a87ec09b
Gabriel Antonio <ga170120@gmail.com>
2024-04-29 20:56:07 -0300

    Editando interface do aluno

--- src/data/@types/alunos.ts
+++ src/data/@types/alunos.ts
@@ -2,7 +2,8 @@ export interface AlunoInterface {
   nome: string;
   email: string;
   data_aula: string | Date;
-  id?: number; // campo opcional.
+  id: number;
+  // ?: campo opcional (pode ser nulo).
 }
 
-export interface AlunoErrorResponseInterface extends AlunoInterface {}
+export interface AlunoErrorResponseInterface extends Omit<AlunoInterface, "id"> {}
